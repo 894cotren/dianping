77dff120aaf2881da5e25bd7bf6e3fed93282137
grey <awc20@qq.com>
2026-01-19 22:52:39 +0800

    chore:升级到mysql8，更新redis配置类

--- src/main/java/com/hmdp/config/RedissonConfig.java
+++ src/main/java/com/hmdp/config/RedissonConfig.java
@@ -3,20 +3,43 @@ package com.hmdp.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.redisson.config.SingleServerConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * redisson 客户端配置； 也可以配置文件去配置
+ * Redisson 客户端配置
+ * 从配置文件中读取 Redis 连接信息
  */
 @Configuration
 public class RedissonConfig {
+
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private String redisPort;
+
+    @Value("${spring.redis.password:}")
+    private String redisPassword;
+
+    @Value("${spring.redis.database:0}")
+    private int redisDatabase;
 
     @Bean
     public RedissonClient redissonClient(){
         // 配置 RedissonClient连接
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://192.168.83.123:6379");
+        String address = "redis://" + redisHost + ":" + redisPort;
+        
+        SingleServerConfig singleServerConfig = config.useSingleServer()
+                .setAddress(address)
+                .setDatabase(redisDatabase);
+        
+        if (redisPassword != null && !redisPassword.isEmpty()) {
+            singleServerConfig.setPassword(redisPassword);
+        }
         // 创建RedissonClient对象
         return Redisson.create(config);
     }
